0c8a700671b17fe2e561724873b4bee0e1ce8791
Marlon Gabriel <marlongabriel1989@gmail.com>
2024-06-15 23:33:59 -0300

    Deltatime Implementation

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -19,8 +19,8 @@ bool Game::IsRunning() const
 
 float projectilePosX = 0.0f;
 float projectilePosY = 0.0f;
-float projectileVelX = 0.1f;
-float projectileVelY = 0.1f;
+float projectileVelX = 20.0f;
+float projectileVelY = 30.0f;
 
 void Game::Initialize(int width, int height)
 {
@@ -83,8 +83,20 @@ void Game::ProcessInput()
 
 void Game::Update()
 {
-	projectilePosX += projectileVelX;
-	projectilePosY += projectileVelY;
+	//Wait until 16ms has ellapsed since the last frame.
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TARGET_TIME));
+
+	// Delta time is the diference in ticks from last frame convcerted to seconds.
+	float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.00f;
+
+	//Clamp deltaTime to a maximum value.
+	deltaTime = (deltaTime > 0.05f) ? 0.05f : deltaTime;
+
+	//Sets the new ticks for the current frame to be used in the next pass.
+	ticksLastFrame = SDL_GetTicks();
+
+	projectilePosX += projectileVelX * deltaTime;
+	projectilePosY += projectileVelY * deltaTime;
 }
 
 void Game::Render()
