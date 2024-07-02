de73434e88644a6cfdc9241c6002b87a44df4310
Marlon Gabriel <marlongabriel1989@gmail.com>
2024-07-02 15:32:11 -0300

    ECS Implementation

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "./Components/TransformComponent.h"
 #include "../libs/glm/glm.hpp"
+
+EntityManager manager;
+SDL_Renderer* Game::renderer;
 
 Game::Game()
 {
@@ -18,8 +22,6 @@ bool Game::IsRunning() const
 	return this->isRunning;
 }
 
-glm::vec2 projectilePos = glm::vec2(0.0f, 0.0f);
-glm::vec2 projectileVel = glm::vec2(20.0f, 20.0f);
 
 void Game::Initialize(int width, int height)
 {
@@ -47,12 +49,21 @@ void Game::Initialize(int width, int height)
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer)
 	{
-		std::cerr << "Eccor creating SDL renderer." << std::endl;
+		std::cerr << "Error creating SDL renderer." << std::endl;
 		return;
 	}
 
+	LoadLevel(0);
+
 	isRunning = true;
 	return;
+}
+
+void Game::LoadLevel(int levelNumber)
+{
+	Entity& newEntity(manager.AddEntity("projectile"));
+	newEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
+
 }
 
 void Game::ProcessInput()
@@ -101,11 +112,7 @@ void Game::Update()
 	//Sets the new ticks for the current frame to be used in the next pass.
 	ticksLastFrame = SDL_GetTicks();
 
-
-	projectilePos = glm::vec2(
-		projectilePos.x + projectileVel.x * deltaTime,
-		projectilePos.y + projectileVel.y * deltaTime
-	);
+	manager.Update(deltaTime);
 }
 
 void Game::Render()
@@ -113,16 +120,13 @@ void Game::Render()
 	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
-	SDL_Rect projectile
+	if (manager.HasNoEntities())
 	{
-		(int)projectilePos.x,
-		(int)projectilePos.y,
-		10,
-		10
-	};
+		return;
+	}
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderFillRect(renderer, &projectile);
+	manager.Render();
+
 
 	SDL_RenderPresent(renderer);
 }
--- 2DGameEngine/src/Game.h
+++ 2DGameEngine/src/Game.h
@@ -4,18 +4,22 @@
 #include<SDL.h>
 #include<SDL_image.h>
 #include<SDL_ttf.h>
+#include "./Entity.h"
+#include "./Component.h"
+#include "./EntityManager.h"
 
 class Game
 {
 	private:
 		bool isRunning;
 		SDL_Window* window;
-		SDL_Renderer* renderer;
 	public:
 		Game();
 		~Game();
 		int ticksLastFrame;
 		bool IsRunning() const;
+		static SDL_Renderer *renderer;
+		void LoadLevel(int levelNumber);
 		void Initialize(int width, int height);
 		void ProcessInput();
 		void Update();
